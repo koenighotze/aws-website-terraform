647f7f58a52dcae4e5f912f7ce1e069fc384731f
David Schmitz <david.schmitz.privat@gmail.com>
2026-03-11 08:49:41 +0100

    Fix shell

--- scripts/set-aws-env.sh
+++ scripts/set-aws-env.sh
@@ -10,5 +10,8 @@ set -o pipefail
 if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 
 unset AWS_PROFILE
-export AWS_ACCESS_KEY_ID=$(op read "op://kh-development/aws-koenighotze-terraform-credentials/AWS_ACCESS_KEY_ID")
-export AWS_SECRET_ACCESS_KEY=$(op read "op://kh-development/aws-koenighotze-terraform-credentials/AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID=$(op read "op://kh-development/aws-koenighotze-terraform-credentials/AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=$(op read "op://kh-development/aws-koenighotze-terraform-credentials/AWS_SECRET_ACCESS_KEY")
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
